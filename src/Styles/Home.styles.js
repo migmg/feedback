2087e621cc55285109ab256caf56a2f5208846e1
miguel oliveira <tsmiguelzin2020@gmail.com>
2024-06-10 01:34:54 +0000

    style

--- src/Styles/Home.styles.js
+++ src/Styles/Home.styles.js
@@ -21,10 +21,99 @@ export const CardContainer = styled.div`
     text-align: center;
    }
 
+   &> button{
+    width: 100% ;
+    padding-block: 1.1rem ;
+    border-radius: 2.25rem;
+    border: none ;
+    font-size: 1.5rem ;
+    font-weight: 700;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    background: ${props => props.theme.submitButtonColor};
+    color: ${props => props.theme.white} ;
+    cursor: pointer;
+    transition: all .3s;
+
+   }
+
+   &:hover{
+    background:  ${props => props.theme.white};
+    color: ${props => props.theme.submitButtonColor};
+
+  }
+
 
 
 `
 
-export const  IconContainer = styled.div``
+export const  IconContainer = styled.div`
+   background:  ${props => props.theme.feedbackBUttonColor};
+   width: 4.8rem;
+   height: 4.8rem;
+   border-radius: 50%;
 
-export const ButtonContainer = styled.div``
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   margin-bottom: 3rem;
+
+
+`
+
+export const ButtonContainer = styled.div`
+  margin-top: 2.4rem;
+  margin-bottom: 3.2rem;
+  display: flex;
+  justify-content: space-between;
+
+  button{
+    width: 1.5rem;
+    height:1.5rem;
+    border-radius: 50%;
+    border: none;
+    background:  ${props => props.theme.feedbackBUttonColor};
+    color: ${props => props.theme.feedbackButtonTex};
+    font-weight: 700;
+    line-height: 2.4rem;
+    cursor: pointer;
+    transition: all .4s;
+  }
+
+
+  &:focus{
+    color: ${props => props.theme.white} ;
+    background:  ${props => props.theme.feedbackBUttonColor};
+
+  }
+
+  &:hover{
+    color: ${props => props.theme.white} ;
+    background:  ${props => props.theme.feedbackBUttonColor};
+
+  }
+
+  & > img {
+    display: block;
+    margin: 3.2rem auto 3.2rem;
+  }
+
+`
+
+export const ResultContainer = styled.p`
+padding: 0.4rem 1.8rem;
+background: ${props => props.theme.feedbackBUttonColor};
+width: fit-content ;
+margin-inline: auto;
+margin-bottom: 3.2rem;
+border-radius: 2.25rem;
+
+
+p {
+    color: ${props => props.theme.submitBUttonColor};
+}
+
+
+
+`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,42 @@
-import { ButtonContainer, CardContainer, IconContainer } from "../Styles/Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "../Styles/Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export  function Home() {
+let aparecerCard = true
   return (
+    aparecerCard === false ? (
     <CardContainer>
       <IconContainer>
-        <img src="iconStarImg" alt="icone estrela" />
+        <img src={iconStarImg} alt="" />
       </IconContainer>
 
       <h1>Como foi o atendimento?</h1>
       <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
       <ButtonContainer>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
       </ButtonContainer>
 
       <button>Enviar</button>
     </CardContainer>
+    ) :(
+      <CardContainer>
+      <img src={thankYouImg} alt="" />
+
+      <ResultContainer>
+        <p>Você selecionou 4 de 5</p>
+      </ResultContainer>
+
+      <h1>obrigado</h1>
+
+      <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+    </CardContainer>
+    )
   )
 }
