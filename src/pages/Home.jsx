74e22bfb8cc7354323b33f85243d8bde2910395d
miguel oliveira <tsmiguelzin2020@gmail.com>
2024-06-10 01:50:45 +0000

    feat: verificação da nota e o envio para trocar de pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "../Styles/Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export  function Home() {
-let aparecerCard = true
+const [mostrarResultado, setmostrarResultado] = useState(false)
+const [feedbackNote, setfeedbackNote] =  useState(0)
+
+function handlefeedbackButtonClick(event){
+  const feedback = Number (event.target.innerText)
+  setfeedbackNote(feedback)
+}
+
+function handleSubmit(){
+  if (feedback === 0) return
+
+  setmostrarResultado(true)
+}
+
+
+
+
+
+
   return (
-    aparecerCard === false ? (
+    mostrarResultado === false ? (
     <CardContainer>
       <IconContainer>
         <img src={iconStarImg} alt="" />
@@ -16,21 +35,21 @@ let aparecerCard = true
       <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
       <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handlefeedbackButtonClick}>1</button>
+        <button onClick={handlefeedbackButtonClick}>2</button>
+        <button onClick={handlefeedbackButtonClick}>3</button>
+        <button onClick={handlefeedbackButtonClick}>4</button>
+        <button onClick={handlefeedbackButtonClick}>5</button>
       </ButtonContainer>
 
-      <button>Enviar</button>
+      <button onClick={handleSubmit}>Enviar</button>
     </CardContainer>
     ) :(
       <CardContainer>
       <img src={thankYouImg} alt="" />
 
       <ResultContainer>
-        <p>Você selecionou 4 de 5</p>
+        <p>Você selecionou {feedbackNote} de 5</p>
       </ResultContainer>
 
       <h1>obrigado</h1>
